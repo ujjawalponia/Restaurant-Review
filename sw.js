01772f3857c81261f709f2ffe0eda93f1bcbffe2
Ujjawal Poonia <ujjawalponia@gmail.com>
2018-10-22 00:31:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,37 @@
-let staticCacheName = 'restaurant-static-v1';
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll([
-                './',
-                './index.html',
-                './restaurant.html',
-                './css/styles.css',
-                './css/responsive.css',
-                './data/restaurants.json',
-                './js/dbhelper.js',
-                './js/main.js',
-                './js/restaurant_info.js',
-                './js/sw_register.js',
-                './img/1.jpg',
-                './img/2.jpg',
-                './img/3.jpg',
-                './img/4.jpg',
-                './img/5.jpg',
-                './img/6.jpg',
-                './img/7.jpg',
-                './img/8.jpg',
-                './img/9.jpg',
-                './img/10.jpg'
-            ]);
-        })
-    );
+self.addEventListener('install', function(event) {
+  var indexPage = new Request('index.html');
+  event.waitUntil(
+    fetch(indexPage).then(function(response) {
+      return caches.open('pwabuilder-offline').then(function(cache) {
+        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        return cache.put(indexPage, response);
+      });
+  }));
 });
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function (cacheNames) {
-                return Promise.all(
-                    cacheNames.filter(function (cacheName) {
-                        return cacheName.startsWith('restaurant-') &&
-                            cacheName != staticCacheName;
-                    }).map(function (cacheName) {
-                        return caches.delete(cacheName);
-                    })
-                );
-            })
-    );
+self.addEventListener('fetch', function(event) {
+  var updateCache = function(request){
+    return caches.open('pwabuilder-offline').then(function (cache) {
+      //only next line added later by ujjawal
+      if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') return;
+      return fetch(request.clone()).then(function (response) {
+        console.log('[PWA Builder] add page to offline'+response.url)
+        return cache.put(request, response);
+      });
+    });
+  };
+
+  event.waitUntil(updateCache(event.request));
+
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+      return caches.open('pwabuilder-offline').then(function (cache) {
+        return cache.match(event.request).then(function (matching) {
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+          return report
+        });
+      });
+    })
+  );
 })
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                return response || fetch(event.request);
-            })
-    );
-});
